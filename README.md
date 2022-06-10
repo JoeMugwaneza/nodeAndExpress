a9b6cec2665dfd684c743575cde993c8c60c225d
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-06-10 06:01:24 +0200

    what hell is going on here

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the place for practicing Nodejs and ExpressJs
---
+++ app.js
@@ -0,0 +1,8 @@
+const john = "John";
+const peter = "Peter";
+
+const sayHi = (name) =>{
+    console.log(`Welcome Ms./Mr. ${name}, ${john}, and ${peter}`);
+}
+
+sayHi('Susan');
