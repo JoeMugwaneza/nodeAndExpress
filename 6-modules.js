07ec29991d5429d809acaacc07bb9abf9c9697cf
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-06-10 22:39:07 +0200

    made few error correction; but confirmation is required

--- 6-modules.js
+++ 6-modules.js
@@ -1,8 +1,10 @@
+
+// OS MODULE
 const os = require('os');
 const user = os.userInfo();
-console.log(user);
+// console.log(user);
 
-console.log(`the system uptime ${os.uptime}`);
+// console.log(`the system uptime ${os.uptime}`);
 
 const currentOS = {
     name: os.type(),
@@ -10,4 +12,17 @@ const currentOS = {
     totalMem:os.totalmem(),
     freeMemo:os.freemem()
 }
-console.log(currentOS)
+// console.log(currentOS)
+
+// PATH MODULE
+const path = require('path');
+// console.log(path.sep);
+
+const filePath = path.join('/content', "subfolder", "test.txt");
+// console.log(filePath);
+
+const base = path.basename(filePath);
+// console.log(base);
+
+const obsulute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt')
+console.log(obsulute);
