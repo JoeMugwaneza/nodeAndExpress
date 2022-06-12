208e7fd55224efb3b7d06d86c1f18bd1c690e3fe
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-06-12 18:39:56 +0200

    setup http module; basic server setup

--- 6-modules.js
+++ 6-modules.js
@@ -1,1 +1,9 @@
-// FILE SYSTEM
+// FILE 
+
+const http = require('http');
+const server = http.createServer((req, res)=>{
+    res.write('Welcome to our home page');
+    res.end();
+})
+
+server.listen(5000);
