06c2731596bbdad37b1abefeb38464677efa331c
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-06-12 19:13:33 +0200

    more advanced features

--- 6-modules.js
+++ 6-modules.js
@@ -1,9 +1,18 @@
 // FILE 
 
 const http = require('http');
-const server = http.createServer((req, res)=>{
-    res.write('Welcome to our home page');
-    res.end();
+const server = http.createServer((req,res)=>{
+    if(req.url ==="/"){
+        res.end('Welcome to our home page');
+    }
+    if(req.url ==="/about"){
+        res.end('Here is our short story')
+    }
+    res.end(`
+    <h1>Ooops!</h1>
+    <p> We can't seem to find the page you are looking for</p>
+    <a href="/">Back Home</a>
+    `)
 })
 
-server.listen(5000);
+server.listen(3000);
