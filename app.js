23b985c3248d21b68ed8bde6b850d5ba596ee6af
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-06-13 05:17:03 +0200

    understanding event loop

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const sayHi = require('./utils.js');
 sayHi('Susan');
 
 const data = require('./4-alternative.js');
-console.log(data);
+// console.log(data);
 
 require('./5-non-invoked');
+
+const {readFile} = require('fs');
+console.log('started a first task');
+readFile('./content/first.txt', 'utf-8',(err, result) =>{
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log(result);
+    console.log('completed first task');
+})
+console.log('starting next task');
+
