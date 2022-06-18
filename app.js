c09c2b094d84cdc1fcac9f4770330ac0e29b063f
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-06-18 19:51:02 +0200

    just looked at how express is easier and quick in routing than nodejs

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-const express = require('express');
+const express = require("express");
+const path = require('path');
+
 const app = express();
 
-app.get('/',(req, res)=>{
-    res.status(200).send('Home Page');
+app.get("/",(req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./nav-app/index.html"))
 });
 
-app.get("/about",(req, res)=>{
-    res.status(200).send('About Page')
-});
+app.use(express.static("./express"));
 
-app.all("*",(req, res)=>{
-    res.status(404).send("<h1> Resource not Found </h1>")
-})
-
-app.listen(5000, ()=>{
-    console.log('App is listening at the port: 5000 .....');
+app.listen(5000,()=>{
+    console.log('Server is listening on port: 5000 ....');
 })
