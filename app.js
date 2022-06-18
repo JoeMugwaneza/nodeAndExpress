7059fcf97ba880c0d48b6954538fd93fa5db7f01
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-06-18 19:55:37 +0200

    reworked on the routing

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const path = require('path');
-
 const app = express();
 
-app.get("/",(req, res)=>{
-    res.sendFile(path.resolve(__dirname, "./nav-app/index.html"))
-});
+app.use(express.static('./express'));
 
-app.use(express.static("./express"));
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./nav-app/index.html"));
+})
 
-app.listen(5000,()=>{
-    console.log('Server is listening on port: 5000 ....');
+app.listen(5000, ()=>{
+    console.log("Serer is listening on port: 5000 ....");
 })
