bf9b95713149580e3099cd5009b6c253461a2ae1
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-06-19 05:28:24 +0200

    interacted with params/queries to search/filter products

--- app.js
+++ app.js
@@ -19,10 +19,36 @@ app.get('/api/products/:productID',(req, res) =>{
     const {productID} = req.params;
     console.log(productID);
     const singleProduct = products.find((product) => product.id === Number(productID))
+    if(!singleProduct){
+        res.status(404).send("<h1>Product does not exit</h1>")
+    }
   
     res.send(singleProduct);
+});
+
+app.get('/api/v1/query', (req, res)=>{
+//    console.log(req.query); 
+   const {search, limit} = req.query;
+
+   let sortedProducts = [...products];
+
+   if(search){
+    sortedProducts = sortedProducts.filter((product) =>{
+        return product.name.startsWith(search)
+    })
+}
+
+if(limit){
+    return sortedProducts = sortedProducts.slice(0, Number(limit))
+}
+if(sortedProducts < 1){
+    // res.status(200).send('Products searched were not found')
+   return res.status(200).json({success: true, data: []})
+}
+res.status(200).json(sortedProducts)
+   res.send("Hello World!");
 })
 
 app.listen(5000, ()=>{
-    console.log('Fuckit, Server is listening on port: 5000 .....');
+    console.log('Fuckit, Server is listening on port: 5000 .....')
 })
