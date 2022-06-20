31c139197fd3f89836ca9ccc2e482b5e7de9d6d2
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-06-20 05:41:17 +0200

    interacted with postman and used it to test the post, get, put and delete methods

--- app.js
+++ app.js
@@ -28,6 +28,39 @@ app.post('/api/people', (req, res)=>{
     res.status(201).json({success: true, person: name})
 })
 
+
+app.put('/api/people/:id', (req, res) =>{
+    const {id} = req.params
+    const {name} = req.body
+    
+    const person = people.find((person) => person.id === Number(id));
+
+    if(!person){
+        return res.status(404).json({success: false, msg:`no person with id ${id}`})
+    }
+
+    const newPeople = people.map((person) =>{
+        if(person.id === Number(id)){
+            person.name = name
+        }
+        return person
+    })
+    res.status(200).json({success: true, data: newPeople})
+})
+
+
+app.delete('/api/people/:id', (req,res)=>{
+    const person = people.find((person)=> person.id === Number(req.params.id))
+
+    if(!person){
+        res.status(404).json({success: false, msg: `no person with id ${id}`})
+    } 
+
+    const newPeople = people.filter((person) => person.id !== Number(req.params.id))
+    return res.status(200).json({success:true, data: newPeople})
+
+})
+
 app.post('/login', (req, res)=>{
     const {name} = req.body;
     if(name){
