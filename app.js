03d51db952efd031eda76eadd6a7aaef3ae7867a
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-06-21 05:09:55 +0200

    looked at how to setup router with expressJS and using the postman to test the routing

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan')
 
-let {products, people} = require('./json/data')
+const people = require('./routes/people');
+const auth = require('./routes/auth');
 
 app.use(morgan('tiny'))
 
@@ -13,62 +14,14 @@ app.use(express.urlencoded({extended: false}))
 // PARSE JSON
 app.use(express.json());
 
-app.get('/api/people', (req, res) =>{
-    res.status(200).send({success: true, data: people});
-    console.log(people);
-});
-
-app.post('/api/people', (req, res)=>{
-    const {name} = req.body
-    if(!name){
-        return res
-        .status(400)
-        .json({success: false, msg: 'Please Provide Name Value'})
-    }
-    res.status(201).json({success: true, person: name})
-})
+app.use('/api/people', people)
+app.use('/login', auth)
 
 
-app.put('/api/people/:id', (req, res) =>{
-    const {id} = req.params
-    const {name} = req.body
-    
-    const person = people.find((person) => person.id === Number(id));
-
-    if(!person){
-        return res.status(404).json({success: false, msg:`no person with id ${id}`})
-    }
-
-    const newPeople = people.map((person) =>{
-        if(person.id === Number(id)){
-            person.name = name
-        }
-        return person
-    })
-    res.status(200).json({success: true, data: newPeople})
-})
 
 
-app.delete('/api/people/:id', (req,res)=>{
-    const person = people.find((person)=> person.id === Number(req.params.id))
 
-    if(!person){
-        res.status(404).json({success: false, msg: `no person with id ${id}`})
-    } 
 
-    const newPeople = people.filter((person) => person.id !== Number(req.params.id))
-    return res.status(200).json({success:true, data: newPeople})
-
-})
-
-app.post('/login', (req, res)=>{
-    const {name} = req.body;
-    if(name){
-        return res.status(200).send(`Welcome ${name}`)
-    }
-
-    res.status(401).send('Please Provide Credentials')
-})
 
 app.listen(5000, ()=>{
     console.log("Fuckit, server is listening on port: 5000 ...");
