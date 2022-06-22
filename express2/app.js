95cd815fc604ecab1382935a7e3c143bb0722f0f
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-06-22 05:59:45 +0200

    worked on postman testing how to get information, added pretty xtension and moment package

--- express2/app.js
+++ express2/app.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const logger = require('./middleware/logger')
+let members = require('./members');
 
 
-const path = require('path')
+// INIT MIDDLEWARE
+app.use(logger);
 
-const PORT = process.env.PORT || 5000;
+// GET ALL MEMBERS
+app.get('/api/members', (req, res) => res.json(members))
 
+
+// GET ONE MEMBER
+
+app.get('/api/members/:id', (req, res) =>{
+    const found = members.some(member => member.id === Number(req.params.id));
+
+    if(found){
+        res.status(200).json(members.filter((member) => member.id === Number(req.params.id)))
+    } else{
+        res.status(400).json({success: false, msg: `No member with the id ${req.params.id} found`})
+    }
+})
 
 app.use(express.static(path.join(__dirname, "public")))
 
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`Fuckit, the server is running on port: ${PORT}`));
---
+++ express2/members.js
@@ -0,0 +1,30 @@
+const members = [
+    { id: 1, 
+        name: 'john',
+        email: 'muganezajoseph@gmail.com',
+        status: 'active'
+    },
+    { id: 2,
+         name: 'peter',
+         email: 'muganezajoseph@gmail.com',
+         status: 'active'
+    },
+    { id: 3,
+         name: 'susan',
+         email: 'muganezajoseph@gmail.com',
+         status: 'active' 
+    },
+    { id: 4,
+         name: 'anna',
+         email: 'muganezajoseph@gmail.com',
+         status: 'active'
+    },
+    { id: 5, 
+        name: 'emma',
+        email: 'muganezajoseph@gmail.com',
+        status: 'active'
+    },
+  ]
+
+
+  module.exports = members;
